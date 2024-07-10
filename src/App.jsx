faf848b363e0fcd7229f059506634293742aacb1
Vruja <140462577+Vruja@users.noreply.github.com>
2024-07-10 10:50:57 +0200

    Update App.jsx
    Delete del carrito otra ves

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,58 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import ShoppingCart from './assets/icons8-carrito-de-compras-50.png'
-import Home from './components/Home'
-import Login from './components/Login'
-import Packages from './components/Packages'
-import Register from './components/Register'
-import ResumeCart from './components/ResumeCart'
-import Dashboard from './components/Dashboard'
-import { Route, Routes, Link} from 'react-router-dom'
-import { useState } from 'react'
-import firebaseConfig from './firebase/firebaseConfig'
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import ShoppingCart from './assets/icons8-carrito-de-compras-50.png';
+import Home from './components/Home';
+import Login from './components/Login';
+import Packages from './components/Packages';
+import Register from './components/Register';
+import ResumeCart from './components/ResumeCart';
+import { Route, Routes, Link } from 'react-router-dom';
+import { useState } from 'react';
+import firebaseConfig from './firebase/firebaseConfig';
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
 
-  const token = sessionStorage.getItem('authToken')
+  // Nº Carrito
+  const cartCount = cartItems.length;
 
-  const clearToken = () => {
-    sessionStorage.removeItem('authToken')
-    location.reload()
-  }
-
-
-  const [cartItems,setCartItems] = useState([])
-
-  {/* VARIABLE-CONTADOR PARA MARCAR CANTIDAD DE ITEMS EN @CartItems DESDE NAVBAR */ }
-  const [cartCount, setCartCount] = useState(0)
-
-  {/* @addToCart RECIBE ITEM DE COMPONENTE @ShoppingCart Y LO INCLUYE EN ARRAY DE @CartItems */ }
+  // Add item to cart
   const addToCart = (item) => {
-    setCartItems([...cartItems, item])
-    setCartCount(cartItems.length +1)
-  }
+    setCartItems([...cartItems, item]);
+  };
 
+  // Remove item from cart
+  const removeFromCart = (index) => {
+    const newCartItems = cartItems.filter((_, i) => i !== index);
+    setCartItems(newCartItems);
+  };
 
   return (
     <>
-      {/* PARTE DE NAVBAR DE APP */ }
       <header className='d-flex justify-content-between align-items-center p-4'>
         <h1>LOGO</h1>
-        <nav id='navNormal'>
+        <nav>
           <ul className='d-flex flex-row justify-content-around'>
             <li className='p-4'><Link to="/home">Home</Link></li>
             <li className='p-4'><Link to="/packages">Packages</Link></li>
             <li className='p-4'><Link to="/login">Login</Link></li>
-            {token ? <li className='p-4'><Link to={"/dashboard"}>Dashboard</Link></li>: ""}
-            {token ? <li className='p-4' onClick={clearToken}>Sing Out</li>: ""}
-            
-
-            {/* SECCION CARRITO EN NAVBAR */ }
             <li className='p-4'>
               <Link to="/resumeCart">
                 <img src={ShoppingCart} alt="ShoppingCart" className='m-1'/>{cartCount}
               </Link>
             </li>
-            
           </ul>
         </nav>
       </header>
-      {/* RUTAS GENERALES DE APLICACIÓN CON PROPS HACIA CADA COMPONENTE HIJO */ }
       <Routes>
         <Route path='/home' element={<Home/>}/>
         <Route path='/packages' element={<Packages addToCart={addToCart}/>}/>
         <Route path='/login' element={<Login/>}/>
-        <Route path='/resumeCart' element={<ResumeCart cartItems={cartItems}/>}/>
+        <Route path='/resumeCart' element={<ResumeCart cartItems={cartItems} removeFromCart={removeFromCart}/>}/>
         <Route path='/register' element={<Register/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
